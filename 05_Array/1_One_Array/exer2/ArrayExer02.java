26728c8a1ab7a1ef35a102ebb896d8b44c376cf6
cworld1 <cworld0@qq.com>
2023-07-08 02:33:24 +0800

    fix: :bug: fix 05-1 code

--- 05_Array/1_One_Array/exer2/ArrayExer02.java
+++ 05_Array/1_One_Array/exer2/ArrayExer02.java
@@ -1,3 +1,5 @@
+package exer2;
+
 import java.util.Scanner;
 
 /**
@@ -15,18 +17,18 @@ import java.util.Scanner;
  */
 public class ArrayExer02 {
     public static void main(String[] args) {
-        //定义包含7个单词的数组
-        String[] weeks = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        // 定义包含7个单词的数组
+        String[] weeks = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 
-        //从键盘获取指定的数值，使用Scanner
-        Scanner scan  = new Scanner(System.in);
+        // 从键盘获取指定的数值，使用Scanner
+        Scanner scan = new Scanner(System.in);
         System.out.println("请输入数值（1-7）：");
         int day = scan.nextInt();
 
-        //针对获取的数据进行判断即可
-        if(day < 1 || day > 7){
+        // 针对获取的数据进行判断即可
+        if (day < 1 || day > 7) {
             System.out.println("你输入的数据有误。");
-        }else{
+        } else {
             System.out.println(weeks[day - 1]);
         }
 
