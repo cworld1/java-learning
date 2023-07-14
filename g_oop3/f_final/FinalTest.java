93a9d9bc8c29a9bf0b1396587f386bd5e044aa84
CWorld <cworld0@qq.com>
2023-07-14 17:50:50 +0800

    feat: :memo: add chapter g section f

---
+++ g_oop3/f_final/FinalTest.java
@@ -0,0 +1,83 @@
+package g_oop3.f_final;
+
+/**
+ * ClassName: FinalTest
+ * Description:
+ *
+ * @Author 尚硅谷-宋红康
+ * @Create 11:34
+ * @Version 1.0
+ */
+public class FinalTest {
+    public static void main(String[] args) {
+        E e = new E();
+        System.out.println(e.MIN_SCORE);
+        // e.MIN_SCORE = 1;
+
+        E e1 = new E(10);
+        System.out.println(e1.LEFT);
+        // e1.LEFT = 11;
+    }
+}
+
+class E {
+    // 成员变量
+    final int MIN_SCORE = 0;
+    final int MAX_SCORE;
+    final int LEFT;
+
+    // final int RIGHT;
+
+    {
+        // MIN_SCORE = 1; // 错误
+        MAX_SCORE = 100;
+    }
+
+    public E() {
+        LEFT = 2;
+    }
+
+    public E(int left) {
+        LEFT = left;
+    }
+
+    // 错误
+    // public void setRight(int right){
+    // RIGHT = right;
+    // }
+
+}
+
+class F {
+    public void method() {
+        final int num;
+        num = 10;
+        // num++;
+        System.out.println(num);
+    }
+
+    public void method(final int num) {
+        // num++;
+        System.out.println(num);
+    }
+}
+
+final class A {
+
+}
+
+// class B extends A{}
+
+// class SubString extends String{}
+
+class C {
+    public final void method() {
+
+    }
+}
+
+class D extends C {
+    // public void method(){
+    //
+    // }
+}
