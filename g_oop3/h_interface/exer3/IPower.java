d2b1e8615459217b36b9bddc8465bc6f797e2264
CWorld <cworld0@qq.com>
2023-07-16 01:34:33 +0800

    feat: :memo: add chapter g section h

---
+++ g_oop3/h_interface/exer3/IPower.java
@@ -0,0 +1,13 @@
+package g_oop3.h_interface.exer3;
+
+/**
+ * ClassName: IPower
+ * Description:
+ *
+ * @Author 尚硅谷-宋红康
+ * @Create 9:18
+ * @Version 1.0
+ */
+public interface IPower {
+    void power();
+}
